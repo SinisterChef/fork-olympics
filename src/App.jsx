76e32fd740185df7ea874f5106b4262ad6f3994a
Kyle OBrien <kyleobis@gmail.com>
2024-12-23 18:35:18 -0500

    env security init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient("https://ugkwxevcxnrglwuysiof.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVna3d4ZXZjeG5yZ2x3dXlzaW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjA2NTAsImV4cCI6MjA1MDQ5NjY1MH0.qtp8r4uhtcE-5OapDDUXvEpu9y44ez5BbIUvt68zvfo");
+import { supabase } from './utils/supabase';
 
 function App() {
   const [countries, setCountries] = useState([]);
