3124aee40876aeeefa56351ab64149af3535c5d0
Kyle OBrien <kyleobis@gmail.com>
2025-02-22 13:42:47 -0500

    homepageFeature refinement

--- project/src/components/homepageFeature.jsx
+++ project/src/components/homepageFeature.jsx
@@ -7,15 +7,15 @@ const homepageFeature = ({ title, image, url, summary }) => {
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="block relative max-w-4xl mx-auto rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300"
+      className="block relative"
     >
-      <div className="relative w-full h-64">
+      <div className="relative w-[80%] h-[475px]">
         <img
           src={image}
           alt={title}
           className="w-full h-full object-cover"
         />
-        <div className="absolute bottom-0 right-0 w-1/4 bg-white p-4 m-4 rounded-lg shadow-lg">
+        <div className="absolute -bottom-[-3rem] -right-[15rem] w-1/2 bg-white p-4 m-4 rounded-lg shadow-lg">
           <h3 className="text-lg font-bold text-darkGray mb-2">{title}</h3>
           <div className="text-sm text-midGray">{parse(summary)}</div>
         </div>
