17827889399fe8cd1651727422523bb0f1c7aa6a
Kyle OBrien <kyleobis@gmail.com>
2025-03-01 12:14:45 -0500

    Removed vite from project

---
+++ project/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/global.css'; // Adjust the path if you renamed the file
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- project/src/utils/directus.js
+++ project/src/utils/directus.js
@@ -1,7 +1,7 @@
 import { createDirectus, rest } from '@directus/sdk';
 
 // Initialize the Directus client with REST transport
-export const directus = createDirectus(import.meta.env.VITE_DIRECTUS_URL).with(rest());
+export const directus = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL).with(rest());
 
 console.log("Initialized Directus client:", directus);
 
