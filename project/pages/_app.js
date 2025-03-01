1ee1ff0a900d1471d4e12846e71e73a0df903fb5
Kyle OBrien <kyleobis@gmail.com>
2025-03-01 12:17:22 -0500

    traditional naming syntax

--- project/pages/_app.js
+++ project/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/global.css'; // Adjust the path if you renamed the file
+import '../styles/globals.css'; // Adjust the path if you renamed the file
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
